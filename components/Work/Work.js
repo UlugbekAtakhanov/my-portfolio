67c6680477b4a8f976967b11c33e4be912c011f4
UlugbekAtakhanov <you@example.com>
2022-06-11 13:47:03 +0500

    June11 13:46

--- components/Work/Work.js
+++ components/Work/Work.js
@@ -5,20 +5,20 @@ import { AppWrapper } from "../AppWrapper";
 import Cards from "./Cards";
 
 const works = [
-	{id: 1, tag: "UI /UX", title: "Gericht Restaurant UI", desc: "UI design of modern restaurant, very creative work. Copied from figma  using Next JS, Tailwind Css.", projectLink: "https://gericht-restaurant-cwd0vm8co-ulugbekatakhanov.vercel.app/", codeLink: "https://github.com/UlugbekAtakhanov/Gericht-Restaurant", img: "/gericht.jpg"},
-	{id: 2, tag: "Next JS", title: "Next-Pizza", desc: "This is a pizza restaurant website. This is a pixel-perfect UI. I used Next.js, Redux Toolkit, API and Tailwind CSS", projectLink: "https://next-pizza.vercel.app/", codeLink: "https://github.com/UlugbekAtakhanov/Next-Pizza", img: "/pizza.png"},
-	{id: 3, tag: "React JS", title: "Modern UI / UX Website", desc: "A modern ui / ux portfolio website", projectLink: "eeee.netlify.app", codeLink: "github", img: "/about01.png"},
-	{id: 4, tag: "React JS", title: "Travel Advisor Google Map", desc: "I created this site just for fun, using react, redux and GoogleMap and Rapid API", projectLink: "https://tr-advisor.netlify.app/", codeLink: "https://github.com/UlugbekAtakhanov/travel-advisor", img: "/googlemap1.png"},
+	{ id: 3, tag: "UI /UX", title: "Mozi TailwindCSS", desc: "Pixel perfect website copyed from figma using tailwindcss", projectLink: "https://mozi-tailwindcss.netlify.app/", codeLink: "https://github.com/UlugbekAtakhanov/Mozi-TailwindCSS", img: "/mozi.png"},
+	{ id: 6, tag: "React JS", title: "React Table", desc: "This table has filtering, pagination and sorting logic without any library, and as a backend I used Firebase", projectLink: "https://react-table-sorting-filtering.netlify.app/", codeLink: "https://github.com/UlugbekAtakhanov/Table-Sorting-Filtering", img: "/table.png"},
 	{id: 5, tag: "Next JS", title: "Mui-project", desc: "This project is created using Mui components. I realized that knowing CSS liblary like Mui, it is a big strength", projectLink: "https://mui-project.vercel.app/", codeLink: "https://github.com/UlugbekAtakhanov/Mui-project", img: "/mui.png"},
-	{id: 6, tag: "React JS", title: "Modern UI / UX Website", desc: "A modern ui / ux portfolio website", projectLink: "eeee.netlify.app", codeLink: "github", img: "/about01.png"},
-	{ id: 7, tag: "UI /UX", title: "Namangan IT School", desc: "This website is created for Namangan(Uzbekistan) IT School. Used technologies: react, sass, tailwindcss and framer motion", projectLink: "https://nits.uz/", codeLink: "https://github.com/UlugbekAtakhanov/nits", img: "/nits21.png"},
 	{id: 8, tag: "Next JS", title: "Modern UI / UX Website", desc: "A modern ui / ux portfolio website", projectLink: "eeee.netlify.app", codeLink: "github", img: "/about01.png"},
+	{id: 1, tag: "UI /UX", title: "Gericht Restaurant UI", desc: "UI design of modern restaurant, very creative work. Copied from figma  using Next JS, Tailwind Css.", projectLink: "https://gericht-restaurant-cwd0vm8co-ulugbekatakhanov.vercel.app/", codeLink: "https://github.com/UlugbekAtakhanov/Gericht-Restaurant", img: "/gericht.jpg"},
 	{id: 9, tag: "React JS", title: "Modern UI / UX Website", desc: "A modern ui / ux portfolio website", projectLink: "eeee.netlify.app", codeLink: "github", img: "/about01.png"},
+	{ id: 7, tag: "UI /UX", title: "Namangan IT School", desc: "This website is created for Namangan(Uzbekistan) IT School. Used technologies: react, sass, tailwindcss and framer motion", projectLink: "https://nits.uz/", codeLink: "https://github.com/UlugbekAtakhanov/nits", img: "/nits21.png"},
+	{id: 4, tag: "React JS", title: "Travel Advisor Google Map", desc: "I created this site just for fun, using react, redux and GoogleMap and Rapid API", projectLink: "https://tr-advisor.netlify.app/", codeLink: "https://github.com/UlugbekAtakhanov/travel-advisor", img: "/googlemap1.png"},
+	{id: 2, tag: "Next JS", title: "Next-Pizza", desc: "This is a pizza restaurant website. This is a pixel-perfect UI. I used Next.js, Redux Toolkit, API and Tailwind CSS", projectLink: "https://next-pizza.vercel.app/", codeLink: "https://github.com/UlugbekAtakhanov/Next-Pizza", img: "/pizza.png"},
 ]
 
 const unique = new Set(works.map(item => item.tag))
 
-
+	
 
 const Work = () => {
 
